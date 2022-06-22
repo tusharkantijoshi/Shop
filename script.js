9fe4cf247e9f0e1cb376d201f7743818304e9bac
Tusharkanti <tusharkantijoshi@gmail.com>
2022-06-22 13:29:43 +0530

    added filter by sizes functionality

--- script.js
+++ script.js
@@ -3,57 +3,108 @@ var productData = {
       {
          "id": 1,
          "title": "Product Name 1",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "L"
       },
       {
          "id": 2,
          "title": "Product Name 2",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "S"
       },
       {
          "id": 3,
          "title": "Product Name 3",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "M"
       },
       {
          "id": 4,
          "title": "Product Name 4",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "M"
       },
       {
          "id": 5,
          "title": "Product Name 5",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "M"
       },
       {
          "id": 6,
          "title": "Product Name 6",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "XL"
       },
       {
          "id": 7,
          "title": "Product Name 7",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "XL"
       },
       {
          "id": 8,
          "title": "Product Name 8",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "XL"
       },
       {
          "id": 9,
          "title": "Product Name 9",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "XL"
       },
       {
          "id": 10,
          "title": "Product Name 10",
-         "price": "$ 78"
+         "price": "$ 78",
+         "size": "S"
+      },
+      {
+         "id": 11,
+         "title": "Product Name 11",
+         "price": "$ 78",
+         "size": "L"
+      },
+      {
+         "id": 12,
+         "title": "Product Name 12",
+         "price": "$ 78",
+         "size": "L"
+      },
+      {
+         "id": 13,
+         "title": "Product Name 13",
+         "price": "$ 78",
+         "size": "M"
+      },
+      {
+         "id": 14,
+         "title": "Product Name 14",
+         "price": "$ 78",
+         "size": "M"
+      },
+      {
+         "id": 15,
+         "title": "Product Name 15",
+         "price": "$ 78",
+         "size": "L"
+      },
+      {
+         "id": 16,
+         "title": "Product Name 16",
+         "price": "$ 78",
+         "size": "S"
+      },
+      {
+         "id": 17,
+         "title": "Product Name 17",
+         "price": "$ 78",
+         "size": "S"
       }
    ]
 }
 
-//! Cart
 const cartIcon = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
 const closeCart = document.querySelector("#close-cart");
@@ -77,7 +128,6 @@ if (document.readyState == 'loading') {
 
 //! ready function
 function ready() {
-
    //! Adding Product Lists
    productData.product.map(data => loadProduct(data));
 
@@ -88,11 +138,14 @@ function ready() {
    //       data.map((loadProduct))
    //    });
 
-   //! Remove items from cart
-   const removeCartButton = document.getElementsByClassName('cart-remove');
-   for (let i = 0; i < removeCartButton.length; i++) {
-      let button = removeCartButton[i];
-      button.addEventListener('click', removeCartItem);
+   //! Initially display all products
+   filterProduct("all");
+
+   //! Add to cart
+   let addCart = document.getElementsByClassName('add-cart');
+   for (let i = 0; i < addCart.length; i++) {
+      let button = addCart[i];
+      button.addEventListener('click', addCartClicked);
    }
 
    //! Quantity Changes
@@ -102,23 +155,22 @@ function ready() {
       input.addEventListener('change', quantityChanged);
    }
 
-   //! Add to cart
-   let addCart = document.getElementsByClassName('add-cart');
-   for (let i = 0; i < addCart.length; i++) {
-      let button = addCart[i];
-      button.addEventListener('click', addCartClicked);
+   //! Remove items from cart
+   const removeCartButton = document.getElementsByClassName('cart-remove');
+   for (let i = 0; i < removeCartButton.length; i++) {
+      let button = removeCartButton[i];
+      button.addEventListener('click', removeCartItem);
    }
 
    //! Buy Button
    document.getElementsByClassName('btn-buy')[0].addEventListener('click', buyButtonClicked);
 };
 
-
 //! Adding Product Lists
 function loadProduct(data) {
 
    let div = document.createElement('div')
-   div.classList.add('pro')
+   div.classList.add('pro', data.size)
 
    let productContainer = document.getElementsByClassName('pro-container')[0]
 
@@ -130,7 +182,7 @@ function loadProduct(data) {
                   <div class="des">
                      <h5 class="product-title">${data.title}</h5>
                      <h4 class="price">${data.price}</h4>
-                     <span>or 9 x $1.21</span>
+                     <span>Size: ${data.size}</span>
                   </div>
 
                   <button class="btn add-cart">
@@ -141,35 +193,30 @@ function loadProduct(data) {
    productContainer.append(div);
 }
 
-//! buyButtonClicked
-function buyButtonClicked(event) {
-   alert('Ordered Placed')
+//! Filter Products by Sizes
+function filterProduct(value) {
+   let buttons = document.querySelectorAll(".size-btn");
+   buttons.forEach((button) => {
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+         console.log(button.innerText);
+         button.classList.add("size-active");
+      } else {
+         button.classList.remove("size-active");
+      }
+   });
 
-   let cartContent = document.getElementsByClassName('cart-content')[0]
-
-   while (cartContent.hasChildNodes()) {
-      cartContent.removeChild(cartContent.firstChild)
-   }
-   updateTotal();
-}
-
-
-
-
-//! Remove Cart Items
-function removeCartItem(event) {
-   let buttonClicked = event.target;
-   buttonClicked.parentElement.remove();
-   updateTotal();
-}
-
-//! Quantity Changed
-function quantityChanged(event) {
-   let input = event.target;
-   if (isNaN(input.value) || input.value <= 0) {
-      input.value = 1;
-   }
-   updateTotal();
+   let elements = document.querySelectorAll(".pro");
+   elements.forEach((element) => {
+      if (value == "all") {
+         element.classList.remove("hide");
+      } else {
+         if (element.classList.contains(value)) {
+            element.classList.remove("hide");
+         } else {
+            element.classList.add("hide");
+         }
+      }
+   });
 }
 
 //! Add to Cart
@@ -231,10 +278,36 @@ function updateTotal() {
       let quantity = quantityElement.value;
 
       total = total + (price * quantity);
-      total = Math.round(total * 100) / 100;
+      total = Math.round(total * 100) / 100; //! for decimal prices
 
       document.getElementsByClassName('total-price')[0].innerText = '$' + total;
    }
-
 }
 
+//! Quantity Changed
+function quantityChanged(event) {
+   let input = event.target;
+   if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
+   }
+   updateTotal();
+}
+
+//! Remove Cart Items
+function removeCartItem(event) {
+   let buttonClicked = event.target;
+   buttonClicked.parentElement.remove();
+   updateTotal();
+}
+
+//! buyButtonClicked
+function buyButtonClicked(event) {
+   alert('Ordered Placed')
+
+   let cartContent = document.getElementsByClassName('cart-content')[0]
+
+   while (cartContent.hasChildNodes()) {
+      cartContent.removeChild(cartContent.firstChild)
+   }
+   updateTotal();
+}
